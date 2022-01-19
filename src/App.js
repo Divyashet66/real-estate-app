d5201849d92873dab7e43fe4a83c6a3d11e1e9a8
Divyashet66 <divyagurunathshet@gmail.com>
2022-01-19 21:28:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import Hero from './components/hero/Hero'
+import Best from './components/best/Best'
+import Featured from './components/featured/Featured'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Best/>
+      <Featured />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/best/Best.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Best.css'
+import Apt1 from '../../assets/apt1.jpg'
+import Apt2 from '../../assets/apt2.jpg'
+import Apt3 from '../../assets/apt3.jpg'
+
+const Best = () => {
+    return (
+        <div className='best'>
+            <h1>Find Best Rated Properties</h1>
+            <div>
+                <p><span className='bold'>All</span></p>
+                <p>Commercial</p>
+                <p>Residential</p>
+                <p>Agricultural</p> 
+            </div>
+            <div className='container'>
+                <img src={Apt1} alt='' />
+                <img src={Apt2} alt='' />
+                <img src={Apt3} alt='' />
+            </div>
+            <button className='btn'>View All</button>
+        </div>
+    )
+}
+
+export default Best
